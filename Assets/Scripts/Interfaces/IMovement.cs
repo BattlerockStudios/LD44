4a2f71d406b5c93e46e0e15181d85d221898397d
ThunderAwesome <troyjsanders@gmail.com>
2019-04-27 19:46:43 -0500

    Added more properties to IMovement

--- Assets/Scripts/Interfaces/IMovement.cs
+++ Assets/Scripts/Interfaces/IMovement.cs
@@ -3,8 +3,14 @@
 public interface IMovement
 {
     MoveState MoveState { get; }
+
+    bool IsOnGround { get; }
+    bool IsMoving { get; }
+
     Vector3 Direction();
     void Rotation();
+
+    Vector3 Velocity { get; }
 }
 
 public enum MoveState
